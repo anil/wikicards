ddd5c0025e6e07dc31e61d7ee5535b31f34f99da
Anil Makhijani <amakhijani@gmail.com>
2009-03-15 21:00:18 -0400

    Add hooks for new version of appengine monkey

---
+++ config.py
@@ -0,0 +1,7 @@
+APP_NAME = 'wikicards.config.middleware:make_app'
+APP_ARGS = ({},)
+APP_KWARGS = dict()
+# You can overwrite these separately for different dev/live settings:
+DEV_APP_ARGS = APP_ARGS
+DEV_APP_KWARGS = APP_KWARGS
+REMOVE_SYSTEM_LIBRARIES = ['webob']
